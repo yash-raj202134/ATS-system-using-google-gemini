b11ce415545d9cc82a2e1321912f0c6ecb7ab018
yash-raj202134 <yashraj3376@gmail.com>
2024-03-14 16:18:11 +0530

    gemini api integration

--- app.py
+++ app.py
@@ -8,4 +8,14 @@ from PIL import Image
 import pdf2image
 import google.generativeai as genai
 
+genai.configure(api_key = os.getenv("GOOGLE_API_KEY"))
 
+                                    
+def get_gemini_response(input,pdf_content,prompt):
+    """
+    This function is used to get the response from the gemini api
+    """
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([input ,pdf_content[0], prompt])
+
+    return response.text                   
