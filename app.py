15309b1df8a22ce7e820be05b6ed412d260f48d5
yash-raj202134 <yashraj3376@gmail.com>
2024-03-14 17:11:19 +0530

    input pdf setup update

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import os
 from PIL import Image
 import pdf2image
 import google.generativeai as genai
-
+import io
+import base64
 genai.configure(api_key = os.getenv("GOOGLE_API_KEY"))
 
                                     
@@ -18,4 +19,35 @@ def get_gemini_response(input,pdf_content,prompt):
     model = genai.GenerativeModel('gemini-pro-vision')
     response = model.generate_content([input ,pdf_content[0], prompt])
 
-    return response.text                   
+    return response.text
+
+
+def input_pdf_setup(uploaded_file):
+    """
+    This function is used to get the pdf content from the uploaded file
+    """
+    if uploaded_file is not None:
+
+        images = pdf2image.convert_from_bytes(uploaded_file.read())
+
+        first_page = images[0]
+
+        # convert to bytes
+        img_byte_arr = io.BytesIO()
+        first_page.save(img_byte_arr, format='JPEG')
+        img_byte_arr = img_byte_arr.getvalue()
+
+        pdf_parts = [
+            {
+                "mime_type": "image/jpeg",
+                "data": base64.b64encode(img_byte_arr).decode()  # to encode to base64
+            }
+        ]
+
+        return pdf_parts
+    
+    else:
+        raise FileNotFoundError("No file uploaded")
+    
+
+
